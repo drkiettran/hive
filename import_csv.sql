628d7fb38fb25798582b0c0e6d87b6a5a9174b4c
Kiet T. Tran, Ph.D <drkiettran@gmail.com>
2020-02-02 12:30:29 -0500

    Updated improt_csv sql script

--- import_csv.sql
+++ import_csv.sql
@@ -2,7 +2,10 @@
 Importing csv file into a table.
 */
 
+drop table if exists carrier;
 
 create table carrier (code string, description string) row format delimited fields terminated by ',';
+
+dfs -cp /user/student/airline/carriers.csv /tmp;
 
 load data inpath '/tmp/carriers.csv' overwrite into table carrier;
